3eedf131d7acb31bbf6ace56bc6c92fbd93df0d2
Pratik Coder <97337836+Pratik219-a@users.noreply.github.com>
2022-02-14 16:42:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+function add() {
+    background_imgTag = new Image();
+    background_ingTag.onload = uploadBackground;
+    background_imgTag.src = background_image;
+
+    rover_imgTag = new Image();
+    rover_ingTag.onload = uploadrover;
+    rover_imgTag.src = rover_image;
+}
+
+function uploadBackground() {
+    AudioContext.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
+}
+
+function uploadrover() {
+    AudioContext.drawImage(rover_imgTag, rover_x, rover_y, rover_width, rover_height);
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+    keyPressed = e.keyCode;
+    console.log(keyPressed);
+        if(keyPressed == '38')
+        {
+            up();
+            console.log("up")
+        }
+        if(keyPressed == '40')
+        {
+            down();
+            console.log("down");
+        }
+        if(keyPressed == '37')
+        {
+            Left();
+            console.log("left");
+        }
+        if(keyPressed == '39')
+        {
+            right();
+            console.log("right");
+        }
+}
+
